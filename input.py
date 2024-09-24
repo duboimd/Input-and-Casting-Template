bd556c34feebd68dd36913f28b27d96a83064b5f
Matthew DuBois <147000029+duboimd@users.noreply.github.com>
2024-09-24 11:36:38 -0600

    Create input.py

---
+++ input.py
@@ -0,0 +1,41 @@
+#Input and Casting Practice
+# Complete the following to demonstrate your learning about input and casting.
+print("Number 1:")
+# 1. Ask the user for their name using input. Then store their name in a variable called name and print their name.
+
+
+print("Number 2:")
+# 2. Ask the user for their age using input and store it in a variable called age. Then print their age.
+
+
+print("Number 3:")
+# 3. Now print a full sentence using the variables name and age and format it using a print statement and commas! Example: Hello _____ (name)! You are ____ (age) years old!
+# Hint: remember we can combine strings and variables in print statements! For example: print(“Hello”,name)
+
+
+print("Number 4:")
+# 4. Ask the user to enter their favourite color using input and store it in a variable called userColor. Then print that color.
+
+
+print("Number 5:")
+# 5. Ask the user to enter their lucky number using input (remember to cast it to an int) then store that in a variable called userNumber. 
+
+
+print("Number 6:")
+# 6. Ask the user for a number using input (remember to cast it to an int), store it in a variable called num and then add 10 to it. Then print num.
+
+
+print("Number 7:")
+# 7. Ask the user for the year that they were born in using input and store it in a variable called birthYear. Then, print their current age. 
+# For example, if you were born in 2010, today you are 12 years old because 2022 - 2010 = 12
+
+
+print("Number 8:")
+# 8. Pretend you are a cashier at a grocery store. Using input, ask the user for their total amount. Calculate the tax on the amount (assuming the tax is 13% -> total*1.13). Print the total including the tax.
+
+
+
+
+
+
+
